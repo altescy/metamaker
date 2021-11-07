a8d67a0b77f7b1ac03af629a03b5e230dc6357f0
altescy <altescy@fastmail.com>
2021-11-07 19:32:46 +0900

    import deploy command

--- metamaker/commands/sagemaker/__init__.py
+++ metamaker/commands/sagemaker/__init__.py
@@ -1,2 +1,3 @@
+from metamaker.commands.sagemaker.deploy import DeployWithSagemakerCommand  # noqa: F401
 from metamaker.commands.sagemaker.sagemaker import SageMakerCommand  # noqa: F401
 from metamaker.commands.sagemaker.train import TrainWithSagemakerCommand  # noqa: F401
