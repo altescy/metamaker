3613ce438d79013aee5325cc3ac0491695689e80
altescy <altescy@fastmail.com>
2021-11-08 22:14:18 +0900

    fix test case

--- tests/test_hyperparameter.py
+++ tests/test_hyperparameter.py
@@ -4,13 +4,13 @@ from metamaker import hyperparameter
 def test_hyperparameter_can_serialize() -> None:
     inputs = {"a": {"b": 1, "c": {"d": [1, 2, 3]}}, "e": 2.718, "f": "foo"}
     output = hyperparameter.serialize(inputs)
-    desired = {"a.b": "1", "a.c.d": "[1, 2, 3]", "e": "2.718", "f": '"foo"', "%%serialized%%": ""}
+    desired = {"a.b": "1", "a.c.d": "[1, 2, 3]", "e": "2.718", "f": '"foo"', "%%serialized%%": "true"}
 
     assert output == desired
 
 
 def test_hyperparameter_can_deserialize() -> None:
-    inputs = {"a.b": "1", "a.c.d": "[1, 2, 3]", "e": "2.718", "f": '"foo"', "%%serialized%%": ""}
+    inputs = {"a.b": "1", "a.c.d": "[1, 2, 3]", "e": "2.718", "f": '"foo"', "%%serialized%%": "true"}
     output = hyperparameter.deserialize(inputs)
     desired = {"a": {"b": 1, "c": {"d": [1, 2, 3]}}, "e": 2.718, "f": "foo"}
 
